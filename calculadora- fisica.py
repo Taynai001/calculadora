31f4dbd7a4d8a6e6def58b85fc85e003359d4e55
Taynai001 <95829356+Taynai001@users.noreply.github.com>
2021-12-08 19:43:23 -0500

    Add files via upload

--- calculadora- fisica.py
+++ calculadora- fisica.py
@@ -2,5 +2,23 @@
     #MEDIDOR DE TERMODINAMICA
 import subprocess
 subprocess.run(["clear"])
+
+print("\t LA TERMODINAMICA ")
 print("\n")
-print(" -> RECUERDA QUE LA TERMODINAMICA ES ")
+print("-> RECUERDA QUE LA TERMODINAMICA ES ")
+print("PD: la temperatura siempre tienes que transformarlo en 'kelvin'")
+print("\n")
+print("ELIJE UNA OPCION")
+print("\n")
+print("1. HALLAR EL VOLUMEN (1)")
+
+n = float(input("introduca el numero"))
+
+if n == 1:
+    print("\n")
+    print("K = 'kelvin'")
+    print("T = 'temperatura'")
+    print("C = 'celcius'")
+    print("\n")
+    print("\t ---> Ley de Charles: Si la temperatura subo el volumen sube y viceversa")
+    print("")
